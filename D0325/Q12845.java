baba4bb599aa9b08015ae450ba5c7b0acbe1d16a
이주연 <white6689@naver.com>
2022-03-25 23:55:42 +0900

    D0325

---
+++ D0325/Q12845.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main (String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String str1[] = br.readLine().split(" ");
+        int max = 0; int maxIndex = 0; int result = 0;
+        for (int i = 0; i < str1.length; i++) { int level = Integer.parseInt(str1[i]);
+            if (max < level) {
+                max = level;
+                maxIndex = i;
+            }
+            result += level;
+        }
+        result += max * (str1.length - 2);
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
